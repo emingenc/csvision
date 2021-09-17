773938265ed9b14b8a8b5ca77d2fca20c9ebbba9
emingenc <emingenc@novit.ai>
2021-09-18 00:07:41 +0300

    FIX: store import fix

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import csvStore from './store/csvStore'
 export default defineComponent({
   name: 'App',
   setup(){
-    provide('csvStore',store)
+    provide('csvStore',csvStore)
   },
 })
 </script>
