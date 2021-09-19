58946143723b71644db4a36d08a0ac329eded592
emingenc <emingenc@novit.ai>
2021-09-19 14:07:19 +0300

    ADD: boot google analytics

--- src/boot/google-analytics.js
+++ src/boot/google-analytics.js
@@ -1,10 +1,7 @@
-import { boot } from 'quasar/wrappers'
 import VueGtag from "vue-gtag";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({router,Vue}) => {
+export default async ({ Vue,router }) => {
   Vue.use(VueGtag, {
-    config: { id: process.env.GOOGLE_ANALYTICS_ID }
-  }, router);
-})
+    config: { id: process.env.GOOGLE_ANALYTICS_ID },
+  },router);
+};
